e16bc042a6fb696a8a732125226ccffb3ae3b0d4
ow.z <ow.z@qq.com>
2022-06-15 23:15:26 +0800

    admin站点

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -8,6 +8,10 @@ class BookInfo(models.Model):
     # 书名
     name = models.CharField(max_length=10)
 
+    # 重写str方法，让admin后台显示书籍名
+    def __str__(self):
+        return self.name
+
 
 class PeopleInfo(models.Model):
     """人物模型[id,name,gender,book]"""
@@ -15,3 +19,7 @@ class PeopleInfo(models.Model):
     gender = models.BooleanField()
     # 外键
     book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
+
+    # 重写str方法，显示字段名
+    def __str__(self):
+        return f"{self.name, self.gender, self.book}"
