fd52386fdec660e8f682859e43e1719136781cfe
ow.z <ow.z@qq.com>
2022-06-17 07:46:54 +0800

    配置DEBUG

--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kzi2wnv+#9206as90$tqu)plc!8(8n%&_ft2kxx&ghg_8xdh72'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
