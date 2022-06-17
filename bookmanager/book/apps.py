3603b43b0321ebc7964c3802edf2d6e27957b49e
ow.z <ow.z@qq.com>
2022-06-17 08:39:26 +0800

    apps配置

--- bookmanager/book/apps.py
+++ bookmanager/book/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class BookConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'book'
+    # 后台应用，用中文显示
+    verbose_name = "图书管理"
