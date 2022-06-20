3f254300c677d57a5a34168f745f1b540fa27138
ow.z <ow.z@qq.com>
2022-06-20 14:41:31 +0800

    djnago-redis

--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -120,3 +120,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 将session保存在redis中
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
+SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+SESSION_CACHE_ALIAS = 'default'
