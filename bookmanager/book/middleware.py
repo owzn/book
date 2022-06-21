f42055eab27868effc2742c121acaa7f7aa3e459
ow.z <ow.z@qq.com>
2022-06-21 10:23:18 +0800

    中间件逻辑

--- bookmanager/book/middleware.py
+++ bookmanager/book/middleware.py
@@ -7,9 +7,16 @@ class TestMiddleware(MiddlewareMixin):
 
     def process_request(self, request):
         """处理请求"""
-        print("每次请求都会执行的中间件")
+        print("每次请求前，都会执行的中间件")
+
+        """处理session"""
+        username = request.session.get('username')
+        if not username:
+            print("没有用户信息")
+            return
+        print('用户信息', username)
 
     def process_response(self, request, response):
         """处理响应"""
-        print("每次响应都会执行的中间件")
+        print("每次响应前，都会执行的中间件")
         return response
